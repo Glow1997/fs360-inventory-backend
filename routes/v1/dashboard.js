d7b3186c5ac6c1cb8972f40d4dd5f0f30f76d760
Glow1997 <jingan.an151@gmail.com>
2020-07-10 16:21:16 +0800

    1st Commit
    10 July

---
+++ routes/v1/dashboard.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+const asyncHandler = require("express-async-handler");
+
+const chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rbg(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rbg(75, 192, 192)',
+    blue: 'rbg(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rbg(201, 203, 207)'
+};
+
+router.get("/revenue", asyncHandler( async function(req, res, next){
+    return res.json({
+        barChartData: {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+            datasets: [
+                {
+                    label: 'Income',
+                    backgroundColor: [
+                        chartColors.purple,
+                        chartColors.purple,
+                        chartColors.purple,
+                        chartColors.purple,
+                        chartColors.purple],
+                    data: [5, 3, 4, 6, 4]
+                }
+            ]
+        },
+        barChartOptions: {
+            responsive: true,
+            legend: {
+                display: false,
+            },
+            title: {
+                display: true,
+                text: 'Monthly Income'
+            },
+            scales: {
+                yAxes: [
+                    {
+                        ticks: {
+                            beginAtZero: true,
+                            suggestedMax: 10
+                        }
+                    }
+                ]          
+            }
+        }
+    })
+}));    
+
+module.exports = router;
